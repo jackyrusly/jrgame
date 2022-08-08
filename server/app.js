fd184d65ebb49f1e96444c542612e9d374babe03
jackyrusly <jackyrusly@gmail.com>
2022-08-08 14:02:37 +0700

    Fix gzip

--- server/app.js
+++ server/app.js
@@ -9,17 +9,17 @@ import Player from './models/player';
 const app = express();
 const server = http.Server(app);
 const io = socket(server);
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4001;
 
 if (port === 4000) {
   const bundler = new Bundler(path.resolve(__dirname, '../client/index.html'));
 
   app.use(bundler.middleware());
 } else {
+  app.use(compression());
   app.use(express.static(path.resolve(__dirname, '../dist')));
 }
 
-app.use(compression());
 
 server.listen(port, () => {
     console.log(`App now listening on port ${port}`);
